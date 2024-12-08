9e105952506d38528d36054e9af2c9d6f60c575d
Dhruv Dewan <dhruvrdewan@gmail.com>
2024-12-08 16:31:43 -0500

    Initializing Server and server shutdown

--- nodeServer.js
+++ nodeServer.js
@@ -3,7 +3,40 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const fs = require("fs");
 const app = express();
+const portNumber = 5000;
 // const { MongoClient, ServerApiVersion } = require('mongodb');
 
 require("dotenv").config({ path: path.resolve(__dirname, 'envVariables/.env') })
+
+app.use(bodyParser.urlencoded({extended:false}));
+process.stdin.setEncoding("utf8");
+
+app.listen(portNumber);
+console.log(`Web server started and running at http://localhost:${portNumber}`);
+
+app.set("views", path.resolve(__dirname, "WebPages"));
+app.set("view engine", "ejs");
+
+app.get("/", (request, response) => {
+    response.render("home");
+});
+
+// Server Shutdown
+
+const prompt = "Type stop to shutdown the server: \n";
+process.stdout.write(prompt);
+process.stdin.on("readable", function () {
+  const dataInput = process.stdin.read();
+  if (dataInput !== null) {
+    const command = dataInput.trim();
+    if (command === "stop") {
+      process.stdout.write("Shutting down the server\n");
+      process.exit(0);
+    } else {
+      process.stdout.write('Invalid command: ' + command + '\n');
+    }
+    process.stdout.write(prompt);
+    process.stdin.resume();
+  }
+});
 
